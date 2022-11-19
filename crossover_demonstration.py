e96201d143ba1929b3b95013000da05994f956b6
Mruzik1 <mimisha2014@gmail.com>
2022-11-19 20:33:40 +0100

    added uniform crossover

--- crossover_demonstration.py
+++ crossover_demonstration.py
@@ -9,9 +9,15 @@ b = ['-']*50
 print(''.join(a))
 print(''.join(b), '\n')
 
+# N-point crossover (could be every number from 1 to the chromosomes_length - 1)
 points = 5
 crossover.n_point(a, b, points)
 print(f"{points}-point crossover: {''.join(crossover.get_offspring())}")
 
-crossover.single_point(a, b)
+# Single point crossover
+crossover.n_point(a, b, 1)
 print(f"Single point crossover: {''.join(crossover.get_offspring())}")
+
+# Uniform crossover
+crossover.uniform_crossover(a, b)
+print(f"Uniform crossover: {''.join(crossover.get_offspring())}")
--- ga/crossover.py
+++ ga/crossover.py
@@ -9,23 +9,22 @@ class Crossover:
     # getting the offspring
     def get_offspring(self) -> list:
         return self.__offspring
-    
-
-    # performs single point crossover
-    def single_point(self, parent1: list, parent2: list):
-        point = randint(1, len(parent1)-1)
-        self.__offspring = parent1[:point] + parent2[point:]
 
 
     # performs n-point crossover
     def n_point(self, parent1: list, parent2: list, points: int):
-        if points >= len(parent1) or points < 1:
-            raise ValueError(f"Wrong points count: {points}.")
-        
         prev_p = 0
+        self.__offspring = []
+
         for i in range(points):
             p = randint(prev_p+1, len(parent1)-points+i)
             self.__offspring += parent2[prev_p:p] if i%2 else parent1[prev_p:p]
             prev_p = p
 
         self.__offspring += parent2[prev_p:] if points%2 else parent1[prev_p:]
+
+
+    # performs uniform crossover
+    def uniform_crossover(self, parent1: list, parent2: list):
+        mask = [randint(0, 1) for _ in range(len(parent1))]
+        self.__offspring = [parent2[i] if mask[i] else parent1[i] for i in range(len(parent1))]
